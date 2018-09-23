8f4177530ea4985aa5eba567a59674b462fd590c
HPBookStumpner\franz.stumpner <franz@stumpner.com>
2018-09-23 14:20:35 +0200

    2018rc4

--- src/main/java/com/stumpner/mediadesk/core/Config.java
+++ src/main/java/com/stumpner/mediadesk/core/Config.java
@@ -62,7 +62,7 @@ public class Config {
 
     //Programm Version Values
 
-    public static String versionNumbner = "2018n20180923";//- a1...n,b1...n,rc1...n,rtm,sr1...n";
+    public static String versionNumbner = "2018rc4";//- a1...n,b1...n,rc1...n,rtm,sr1...n";
     public static String versionDate = "2018-09-23";
     public static String SERIAL_UID = "2018092301";
 
